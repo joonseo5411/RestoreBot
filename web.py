0008dda7a6f7ba785c9f3bfc339db625f5bbfb59
joonseo5411 <cheonjoonseo541@gmail.com>
2025-01-24 20:51:56 +0900

    web update

--- web.py
+++ web.py
@@ -46,10 +46,4 @@ async def callback():
     
 
 
-def run():
-    app.run(host="0.0.0.0", port=4404)
-    
-def web():
-    web = threading.Thread(target=run, args=())
-    web.start()
-    web.join()
+app.run(host="0.0.0.0", port=4404)

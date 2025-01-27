5279419ddb6d26edca4b2e5e547848995e0d68a3
joonseo5411 <cheonjoonseo541@gmail.com>
2025-01-27 15:16:40 +0900

    test: Set

--- function/button.py
+++ function/button.py
@@ -12,7 +12,7 @@ class settingBtn:
         data = await DB.getGuildInfo(self.i.guild_id)
         self.data = data
         print(data)
-        class roleBtn(discord.ui.View):
+        class SetBtn(discord.ui.View):
             def __init__(self):
                 super().__init__(timeout=None)
 
@@ -23,12 +23,19 @@ class settingBtn:
             @discord.ui.button(label="ㅣ웹훅설정", emoji="💬", style=discord.ButtonStyle.blurple)
             async def webhookSetting(self, i: discord.Interaction, btn: discord.ui.Button):
                 pass
+
+        role = self.i.guild.get_role(int(data[2])) if data[2] != None else False
+        roleName = "Setting need" if not role else role.name
+        roleID = "Setting need"if not role else role.id
             
         embed = discord.Embed(
             title="복구봇 설정하기",
             description=f"🔰ㅣ역할\n```ansi\n🔰 [2;32m설정된 역할[0m: {roleName}\n🔰 [2;32m[2;33m역할 아이디[0m[2;32m[0m: {roleID}\n```\n💬ㅣ웹훅\n```ansi\n💬 [2;34m인증로그[0m: {'설정필요' if not data[1][0] else data[1][0]}\n```",
             color=discord.Color.green()
         )
+
+        await self.i.response.send_message(embed=embed, view=SetBtn())
+        self.response = await self.i.original_response()
 
     async def roleSetting(self):
         pass

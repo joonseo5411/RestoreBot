e296c0dfba4688600ef4b00bcd8fd724b95df4d9
mir231 <cheonjoonseo541@gmail.com>
2025-01-27 15:06:11 +0900

    button.py 설정 커맨드 추가

--- function/button.py
+++ function/button.py
@@ -16,17 +16,17 @@ class settingBtn:
             def __init__(self):
                 super().__init__(timeout=None)
 
-            @discord.ui.button(label="", emoji="", style=discord.ButtonStyle.blurple)
+            @discord.ui.button(label="ㅣ역할설정", emoji="🔰", style=discord.ButtonStyle.blurple)
             async def roleSetting(self, i: discord.Interaction, btn: discord.ui.Button):
                 pass
 
-            @discord.ui.button(label="", emoji="", style=discord.ButtonStyle.blurple)
+            @discord.ui.button(label="ㅣ웹훅설정", emoji="💬", style=discord.ButtonStyle.blurple)
             async def webhookSetting(self, i: discord.Interaction, btn: discord.ui.Button):
                 pass
             
         embed = discord.Embed(
-            title="설정",
-            description="",
+            title="복구봇 설정하기",
+            description=f"🔰ㅣ역할\n```ansi\n🔰 [2;32m설정된 역할[0m: {roleName}\n🔰 [2;32m[2;33m역할 아이디[0m[2;32m[0m: {roleID}\n```\n💬ㅣ웹훅\n```ansi\n💬 [2;34m인증로그[0m: {'설정필요' if not data[1][0] else data[1][0]}\n```",
             color=discord.Color.green()
         )
 
